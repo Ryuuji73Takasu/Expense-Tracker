6d41f548c068ec83968e477f801a045d2489330a
Ryuuji73Takasu <mrh.sourav54@gmail.com>
2021-08-16 16:19:01 +0600

    Final Commit

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,22 @@
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpenses } from './components/IncomeExpenses'
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction /> 
+    </GlobalProvider>
+  );
+}
+
+export default App;
